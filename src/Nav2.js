3b72a997c3ab62e5bfc1acf33a6269f4ee35329c
Sandman0083 <anirbanbhattacharya@amceducation.in>
2023-02-13 18:04:07 +0530

    Update Nav2.js

--- src/Nav2.js
+++ src/Nav2.js
@@ -5,7 +5,7 @@ function Nav2(){
             <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+      <img src="https://photos.google.com/search/_tra_/photo/AF1QipMJGQHkZf8yixJ6DRBcutZKN3XcrzdFwsthOA1j" alt="" width="30" height="24" class="d-inline-block align-text-top" />
       Bootstrap
     </a>
   </div>
